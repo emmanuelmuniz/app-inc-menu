f0edb15602f4a2490764b1c021d1d9e47569b787
emmanuelmuniz <munizadaroemmanuel@gmail.com>
2024-09-30 10:35:16 -0300

    WIP

--- app/edition/categories/page.jsx
+++ app/edition/categories/page.jsx
@@ -58,22 +58,40 @@ export default function Categories() {
                 ) : (
 
                     <div className="flex flex-col md:flex-row place-content-between bg-white">
-                        <div className="flex md:w-4/12 my-2 md:flex-row">
-                            <Tabs className="flex principal-tabs w-full ml-2" defaultIndex={0} onSelect={(index) => handleSectionSelect(sections[index]._id)}>
-                                <TabList className="text-md text-black px-2 mr-2 rounded-sm w-full bg-gray-2 min-h-[calc(100vh-6.3rem)]">
-                                    <div className="mt-2 mb-3 font-semibold">Categorías</div>
-                                    {sections.map((section) => (
-                                        <Tab key={section._id} className="flex tab text-md bg-ghost-white cursor-pointer p-2 my-1 rounded-sm transition">
-                                            <div className="flex justify-center items-center w-[13px] h-6 mr-2 cursor-grab">
-                                                <div className="flex flex-col justify-center w-[13px] h-full">
-                                                    <div className="mb-1 h-[2px] bg-gray-3 rounded-"></div>
-                                                    <div className="h-[2px] bg-gray-3 rounded"></div>
-                                                </div>
+                        <div className="flex md:w-7/12 my-2 md:flex-row">
+                            <Tabs className="flex principal-tabs  w-full ml-2" defaultIndex={0} onSelect={(index) => handleSectionSelect(sections[index]._id)}>
+                                <TabList className="text-md rounded-lg overflow-hidden text-black px-2 mr-2 w-full bg-gray-2 min-h-[calc(100vh-6.3rem)]">
+                                    <div className="rounded-sm overflow-hidden">
+                                        <div>
+                                            <div className="flex p-2 mt-2 bg-white border-b-2 border-gray rounded-t-sm">
+                                                <div className="w-1/12 ml-2"></div>
+                                                <div className="w-7/12">Categoría</div>
+                                                <div className="w-4/12">Estado</div>
                                             </div>
-
-                                            {section.name_es}
-                                        </Tab>
-                                    ))}
+                                        </div>
+                                        <div>
+                                            {sections.map((section) => (
+                                                <div>
+                                                    <Tab key={section._id} className="flex tab text-md px-2 py-1 bg-white cursor-pointer transition">
+                                                        <div className="w-1/12 ml-2">
+                                                            <div className="flex justify-center items-center w-[13px] h-6 cursor-grab">
+                                                                <div className="flex flex-col justify-center w-[13px] h-full">
+                                                                    <div className="mb-1 h-[2px] bg-gray-3 rounded-"></div>
+                                                                    <div className="h-[2px] bg-gray-3 rounded"></div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div className="w-7/12">
+                                                            {section.name_es}
+                                                        </div>
+                                                        <div className="w-4/12">
+                                                            Activo
+                                                        </div>
+                                                    </Tab>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
                                 </TabList>
                                 <div className=" text-md w-full rounded-sm bg-gray-2 px-2">
                                     <div className="mt-2 mb-3 font-semibold">Subcategorías</div>
@@ -103,7 +121,7 @@ export default function Categories() {
                             <div className="w-[2px] mx-2 bg-gray h-full"></div>
                         </div>
 
-                        <div className="flex flex-col md:flex-row md:w-8/12 p-2 pl-0 rounded-sm w-full">
+                        <div className="flex flex-col md:flex-row md:w-5/12 p-2 pl-0 rounded-sm w-full">
                             <div className="md:w-1/2 w-full h-full flex">
                                 <div className="w-full bg-white p-2 rounded-sm">
                                     <div className="border-gray">Categoría:</div>
