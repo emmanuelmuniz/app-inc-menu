bb5017b5e46a91e1569914de823a78c899614eac
emmanuelmuniz <munizadaroemmanuel@gmail.com>
2024-10-14 12:28:08 -0300

    New menu design

--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -58,6 +58,16 @@ export default function DigitalMenu() {
         setSelectedCategoryId(filteredCategories[0]._id);
     }
 
+    useEffect(() => {
+        const handleContextMenu = (e) => {
+            e.preventDefault();
+        };
+        document.addEventListener('contextmenu', handleContextMenu);
+        return () => {
+            document.removeEventListener('contextmenu', handleContextMenu);
+        };
+    }, []);
+
     return (
         <>
             {loading ? (
@@ -67,7 +77,7 @@ export default function DigitalMenu() {
             ) : (
                 <div className="pt-3 w-full min-h-[calc(100vh-5.5rem)]">
                     <Tabs className="principal-tabs" defaultIndex={0} onSelect={(index) => handleSectionSelect(sections[index]._id)}>
-                        <TabList className="flex flex-wrap text-xl text-black mt-2 w-full px-11 pb-3">
+                        <TabList className="flex flex-wrap text-xl text-black mt-2 w-full px-5 pb-3">
                             {sections.map((section) => (
                                 <Tab key={section._id} className="tab bg-ghost-white focus:outline-none cursor-pointer mr-2 mb-2 p-1 px-4 rounded-sm transition">
                                     {section.name_es}
@@ -78,7 +88,7 @@ export default function DigitalMenu() {
                             {sections.map((section) => (
                                 <TabPanel key={section._id} className="w-full">
                                     <Tabs className="w-full secondary-tabs" defaultIndex={0}>
-                                        <TabList className="flex flex-wrap px-11 pb-2">
+                                        <TabList className="flex flex-wrap px-5 pb-2">
                                             {categories.filter(category => category.section._id === section._id)
                                                 .map((sectionCategory) => (
                                                     <Tab key={sectionCategory._id}
@@ -89,33 +99,38 @@ export default function DigitalMenu() {
                                                 ))}
                                         </TabList>
                                         <div className="border-b-1 border-silver w-full"></div>
-                                        <div className="px-11 text-black">
+                                        <div className="px-5 text-black grid gap-4 md:grid-cols-2">
                                             {products
-                                                .filter(product => product.category._id === selectedCategoryId)
+                                                .filter((product) => product.category._id === selectedCategoryId)
                                                 .map((product) => (
-                                                    <div key={product._id} className="text-2xl border-b-1 border-silver py-5">
-                                                        <span className="text-black mr-2">
-                                                            {product.name_es} -
-                                                            <span className="text-inc-light-blue"> ${product.price}</span>
-                                                        </span>
-                                                        <p className='text-lg mt-3'>{product.description_es}</p>
+                                                    <div key={product._id} className="my-4 shadow-sm rounded-sm overflow-hidden">
                                                         {product.image && product.image.url && (
-                                                            <div className='relative w-full h-full'>
+                                                            <div className='relative w-full h-52'>
                                                                 {imageLoading && (
-                                                                    <div className='absolute top-0 left-0 h-52 w-full bg-silver animate-pulse rounded-sm'></div>
+                                                                    <div className='absolute top-0 left-0 h-full w-full bg-silver animate-pulse'></div>
                                                                 )}
                                                                 <img
-                                                                    className={`rounded-sm h-72 w-full object-cover transition-opacity duration-500 ${imageLoading ? 'opacity-0' : 'opacity-100'
+                                                                    className={`h-full w-full object-cover transition-opacity duration-500 ${imageLoading ? 'opacity-0' : 'opacity-100'
                                                                         }`}
                                                                     src={product.image.url}
                                                                     alt={product.name_es}
                                                                     onLoad={() => setImageLoading(false)}
+                                                                    onContextMenu={(e) => e.preventDefault()}
+                                                                    draggable='false'
                                                                 />
                                                             </div>
                                                         )}
+                                                        <div className="p-4 md:p-2">
+                                                            <span className="text-black text-bold text-2xl md:text-lg">
+                                                                {product.name_es} -
+                                                                <span className="text-inc-light-blue"> ${product.price}</span>
+                                                            </span>
+                                                            <p className='text-xl md:text-base mt-2'>{product.description_es}</p>
+                                                        </div>
                                                     </div>
                                                 ))}
                                         </div>
+
                                     </Tabs>
                                 </TabPanel>
                             ))}
