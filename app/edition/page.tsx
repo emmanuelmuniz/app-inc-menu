67c1685073ef683a7b69a109f436abebe65a414b
emmanuelmuniz <munizadaroemmanuel@gmail.com>
2024-10-20 13:03:13 -0300

    Remove unnecessary code

--- app/edition/page.tsx
+++ app/edition/page.tsx
@@ -6,11 +6,6 @@ import { useSession } from "next-auth/react";
 export default function Edition() {
     const { data: session, status } = useSession();
 
-    // Mientras se carga la sesión, puedes mostrar un indicador de carga opcionalmente
-    if (status === "loading") {
-        return <p>Cargando...</p>;
-    }
-
     return (
         <div className="h-full bg-white">
             <Editor />
