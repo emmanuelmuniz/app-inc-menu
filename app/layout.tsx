c88dfa70da01e149528896427702b41c8db13fdd
emmanuelmuniz <munizadaroemmanuel@gmail.com>
2024-10-20 23:44:42 -0300

    Increment menu width

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,10 +27,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={gratelos.className}>
         <div className="layout-container flex flex-col justify-center items-center min-h-screen no-scrollbar font-gratelos no-select">
-          <div className="layout-container-2 bg-white rounded shadow-lg w-full sm:w-6/12 
+          <div className="layout-container-2 bg-white rounded shadow-lg w-full sm:w-7/12 
                     sm:min-h-[calc(100vh-1.5rem)] sm:max-h-[calc(100vh-2.5rem)]
                     sm:rounded sm:shadow-lg h-full min-h-screen overflow-y-auto no-scrollbar relative">
-            <div className="fixed z-10 bg-white w-full sm:w-6/12 rounded">
+            <div className="fixed z-10 bg-white w-full sm:w-7/12 rounded">
               <Navbar />
             </div>
             <div className="mt-[calc(4rem+1px)] children-div">
