3625ffd65c3d4c75ea0e5af77727c7fbaf7e41a9
emmanuelmuniz <munizadaroemmanuel@gmail.com>
2024-10-21 13:25:57 -0300

    Fix page height

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,12 +29,12 @@ export default function RootLayout({
         <div className="layout-container relative flex flex-col justify-center items-center 
                         bg-[url('/images/inc-front.jpeg')] bg-fixed bg-contain h-screen
                         min-h-screen no-scrollbar font-gratelos no-select">
-          <div className="absolute inset-0" style={{ backdropFilter: 'blur(1px)' }}></div>
+          <div className="absolute inset-0 bg-gray-3/30" style={{ backdropFilter: 'blur(1px)' }}></div>
 
-          <div className="layout-container-2 relative z-10 bg-white rounded shadow-lg w-full sm:w-6/12 
-                    sm:min-h-[calc(100vh-1.5rem)] sm:max-h-[calc(100vh-2.5rem)]
-                    sm:rounded sm:shadow-lg h-full min-h-screen overflow-y-auto no-scrollbar">
-            <div className="fixed z-10 bg-white w-full sm:w-6/12 rounded">
+          <div className="layout-container-2 relative z-10 bg-white rounded shadow-lg w-full md:w-6/12 
+                    md:min-h-[calc(100vh-1.5rem)] md:max-h-[calc(100vh-2.5rem)]
+                    md:rounded md:shadow-lg h-full min-h-screen overflow-y-auto no-scrollbar">
+            <div className="fixed z-10 bg-white w-full md:w-6/12 rounded">
               <Navbar />
             </div>
             <div className="mt-[calc(4rem+1px)] children-div">
