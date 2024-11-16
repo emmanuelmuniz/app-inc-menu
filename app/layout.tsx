ba2373e0c549bb7a128773db1b8c7a7f56c17f4b
emmanuelmuniz <munizadaroemmanuel@gmail.com>
2024-11-16 18:32:55 -0300

    Trying to fix y margins cause scrolls (can't test in local)

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,14 +30,13 @@ export default function RootLayout({
                         bg-[url('/images/inc-front.jpeg')] bg-fixed bg-contain h-screen
                         min-h-screen no-scrollbar font-gratelos no-select">
           <div className="absolute inset-0 bg-gray-3/30" style={{ backdropFilter: 'blur(1px)' }}></div>
-
           <div className="layout-container-2 relative z-10 bg-white rounded shadow-lg w-full md:w-6/12 
-                    md:min-h-[calc(100vh-1.5rem)] md:max-h-[calc(100vh-2.5rem)]
-                    md:rounded md:shadow-lg h-full min-h-screen overflow-y-auto no-scrollbar">
-            <div className="fixed z-10 bg-white w-full md:w-6/12 rounded">
+      md:min-h-[calc(100vh-1.5rem)] md:max-h-[calc(100vh-2.5rem)]
+      md:rounded md:shadow-lg h-full min-h-screen overflow-hidden">
+            <div className="sticky top-0 z-10 bg-white w-full">
               <Navbar />
             </div>
-            <div className="my-[calc(4rem+4px)] children-div">
+            <div className="h-full overflow-y-auto">
               {children}
             </div>
           </div>
