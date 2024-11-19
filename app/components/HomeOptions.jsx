54f180b819d82ca68c4d7f16b9c8979260a5f123
emmanuelmuniz <munizadaroemmanuel@gmail.com>
2024-11-18 21:40:49 -0300

    Fix top margin

--- app/components/HomeOptions.jsx
+++ app/components/HomeOptions.jsx
@@ -45,7 +45,7 @@ export default function HomeOptions() {
                 </div>
             </div>
             <a className="" href="https://instagram.com/ineedcoffee.ar" target="_blank" rel="noopener noreferrer">
-                <div className="grid grid-cols-1 text-center text-xl px-1 cursor-pointer mb-1">
+                <div className="grid grid-cols-1 text-center text-xl px-1 cursor-pointer mb-1 h-full">
                     <div className="w-full px-16 py-16 bg-silver relative">Seguinos en redes sociales y conseguí premios
                         y descuentos especiales pensados para vos.
                         <div className="absolute inset-0 bg-black opacity-0 hover:opacity-20 transition-opacity duration-300">
--- app/layout.tsx
+++ app/layout.tsx
@@ -30,13 +30,14 @@ export default function RootLayout({
                         bg-[url('/images/inc-front.jpeg')] bg-fixed bg-contain h-screen
                         min-h-screen no-scrollbar font-gratelos no-select">
           <div className="absolute inset-0 bg-gray-3/30" style={{ backdropFilter: 'blur(1px)' }}></div>
+
           <div className="layout-container-2 relative z-10 bg-white rounded shadow-lg w-full md:w-6/12 
-      md:min-h-[calc(100vh-1.5rem)] md:max-h-[calc(100vh-2.5rem)]
-      md:rounded md:shadow-lg h-full min-h-screen">
+                    md:min-h-[calc(100vh-1.5rem)] md:max-h-[calc(100vh-2.5rem)]
+                    md:rounded md:shadow-lg h-full min-h-screen overflow-y-auto no-scrollbar">
             <div className="fixed z-10 bg-white w-full md:w-6/12 rounded">
               <Navbar />
             </div>
-            <div className="pt-[72px]">
+            <div className="children-div pt-[4.5rem]">
               {children}
             </div>
           </div>
